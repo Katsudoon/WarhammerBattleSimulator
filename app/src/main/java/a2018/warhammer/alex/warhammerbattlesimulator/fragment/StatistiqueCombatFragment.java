b72237fcba24682909c3e3366c8e46cc1dfe8813
Alexis Gauthier <alexgauth.work@gmail.com>
2018-07-13 14:03:04 +0200

    better

--- app/src/main/java/a2018/warhammer/alex/warhammerbattlesimulator/fragment/StatistiqueCombatFragment.java
+++ app/src/main/java/a2018/warhammer/alex/warhammerbattlesimulator/fragment/StatistiqueCombatFragment.java
@@ -2,6 +2,7 @@ package a2018.warhammer.alex.warhammerbattlesimulator.fragment;
 
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,10 @@ import a2018.warhammer.alex.warhammerbattlesimulator.R;
 
 public class StatistiqueCombatFragment extends Fragment {
 
+    private View view;
+    private Context context;
+    private String unite1, unite2,vainqueur;
+
 
     public StatistiqueCombatFragment() {
 
@@ -21,8 +26,23 @@ public class StatistiqueCombatFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_statistique_combat, container, false);
+        view = inflater.inflate(R.layout.fragment_combat, container, false);
+        context = view.getContext();
+        initialize();
+        setText();
+        return view;
     }
+
+    public void setText() {
+
+    }
+
+    public void initialize(){
+        Bundle data = this.getArguments();
+        unite1 = data.getString("unite1");
+        unite2 = data.getString("unite2");
+        vainqueur = data.getString("vainqueur");
+    }
+
 
 }
